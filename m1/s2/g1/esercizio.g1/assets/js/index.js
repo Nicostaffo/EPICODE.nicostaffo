13eb405105771378118034f0cc5da2e3b59391dc
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-04-03 16:59:23 +0200

    first.commit

---
+++ m1/s2/g1/esercizio.g1/assets/js/index.js
@@ -0,0 +1,22 @@
+//alert('Usa il pop up per dare dei messaggi');
+//window.alert('Sto studiando JS');
+
+//prova utilizzo funzione console log js
+var a = 5;
+var b = 6;
+console.log(a + b);
+
+//prova utilizzo if con condizione molteplice
+if ((a + b) > 10 && (a + b) > 8){
+    document.write('true')
+} else {                     //true su pagina
+    console.log('false')
+}
+
+if ((a + b) > 11 && (a + b) > 15){
+    document.write('true')
+} else {                     //false su console 
+    console.log('false')
+}
+
+document.getElementById()
