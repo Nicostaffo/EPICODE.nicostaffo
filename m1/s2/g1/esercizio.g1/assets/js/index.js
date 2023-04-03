99df96f6fb28152b0df8348a8e4ed0b32493a06d
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-04-03 22:02:36 +0200

    perle.del.sensei.gianluca

--- m1/s2/g1/esercizio.g1/assets/js/index.js
+++ m1/s2/g1/esercizio.g1/assets/js/index.js
@@ -2,21 +2,25 @@
 //window.alert('Sto studiando JS');
 
 //prova utilizzo funzione console log js
-var a = 5;
-var b = 6;
-console.log(a + b);
+// var a = 5;
+// var b = 6;
+// console.log(a + b);
 
 //prova utilizzo if con condizione molteplice
-if ((a + b) > 10 && (a + b) > 8){
-    document.write('true')
-} else {                     //true su pagina
-    console.log('false')
-}
+// if ((a + b) > 10 && (a + b) > 8){
+//     document.write('true')
+// } else {                     //true su pagina
+//     console.log('false')
+// }
 
-if ((a + b) > 11 && (a + b) > 15){
-    document.write('true')
-} else {                     //false su console 
-    console.log('false')
-}
+// if ((a + b) > 11 && (a + b) > 15){
+//     document.write('true')
+// } else {                     //false su console 
+//     console.log('false')
+// }
 
-document.getElementById()
+// //accensione e spegnimento lampadina
+
+// function spegni(){
+//     document.getElementById("bulb-off").src = "/img/lampadina-spenta.svg"
+// }
--- m1/s2/g1/esercizio.g1/assets/js/naming.js
+++ m1/s2/g1/esercizio.g1/assets/js/naming.js
@@ -3,14 +3,14 @@
 
 /*var pet = 'gatto';
 var Pet = 'cane';*/
-var petPreferito = 'criceto';
-var PetPreferito = 'coniglio';
+// var petPreferito = 'criceto';
+// var PetPreferito = 'coniglio';
 
-var $pet = 'giraffa';
-console.log($pet);
-var _pet = 'leone';
-console.log(_pet);
-var _pet2 = 'pantera';
-document.write(_pet2);
+// var $pet = 'giraffa';
+// console.log($pet);
+// var _pet = 'leone';
+// console.log(_pet);
+// var _pet2 = 'pantera';
+// document.write(_pet2);
 
-//var 12 = 'numero';
+// //var 12 = 'numero';
