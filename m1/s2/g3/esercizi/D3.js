7a548fc9c990c442f82d7ca25624ed963645a115
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-04-05 18:54:35 +0200

    last.commit.g3

--- m1/s2/g3/esercizi/D3.js
+++ m1/s2/g3/esercizi/D3.js
@@ -194,10 +194,10 @@ console.log('------------------separazione----------------------')
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let val = 7
-if (val >= 5 && val < 10) {
-  console.log("Meno di 10");
-} else if (val < 5) {
+if (val < 5) {
   console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
 } else {
   console.log("Uguale a 10 o maggiore");
 }
