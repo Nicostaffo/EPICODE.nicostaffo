f1119e9188fa2e36e6847d7cd0055981c38bf765
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-04-08 15:40:24 +0200

    correzioni.per.me

--- m1/s2/g5/esame.s2/D5_v3/D5.js
+++ m1/s2/g5/esame.s2/D5_v3/D5.js
@@ -11,8 +11,9 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
-console.log(pets);
-
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i]);
+}
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
