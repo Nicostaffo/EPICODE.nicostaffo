bedcb4a29f89e4280f48d971a622e9b1ed04e7fe
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-05-10 17:41:28 +0200

    commit homeworks

---
+++ m2/s3/g3/fetch.js
@@ -0,0 +1,52 @@
+fetch('https://striveschool-api.herokuapp.com/books')
+    .then((res) => {
+        if (res.ok) {
+            return res.json()
+        } else {
+            return new Error("error")
+        }
+    })
+    .then((list) => {
+        let bookList = document.querySelector('.row')
+
+        list.forEach((book) => {
+            let col = document.createElement('div');
+            col.classList.add('col-sm-10', 'col-md-5', 'col-lg-4');
+            col.innerHTML = `<div class="card">
+            <img id="bookImg" src=${book.img} class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <p class = "card-text">${book.price}</p>
+                <div class="d-flex">
+                <a class="btn btnDelete btn-primary">Remove book</a>
+                <a class="btn btnAddToCart btn-danger">Add to cart</a>
+                </div>
+            </div>
+        </div>`;
+            bookList.appendChild(col);
+
+            let btnDelete = col.querySelectorAll('.btnDelete');
+            btnDelete.forEach((a) => {
+                a.addEventListener('click', function () {
+                    let card = this.closest('.card');
+                    card.classList.remove('d-flex');
+                    card.classList.add('d-none');
+                });
+            });
+            let car = document.querySelector('ul')
+            let btnAddToCart = col.querySelectorAll('.btnAddToCart');
+            btnAddToCart.forEach((b) => {
+                b.addEventListener('click', () => {
+                    let card = this.closest('.card');
+                    let newLi = document.createElement('li');
+                    newLi.innerHTML = `<p>${book.title}</p> <p>${book.price}</p>`;
+                    car.appendChild(newLi);
+                });
+            });
+        });
+    })
+
+
+    .catch((err) => {
+        console.log(err);
+    })
