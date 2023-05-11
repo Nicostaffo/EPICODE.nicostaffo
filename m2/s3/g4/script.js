77376b9d600d5b861a77beed143c0d8f0ad1b6f7
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-05-11 17:39:29 +0200

    commit

--- m2/s3/g4/script.js
+++ m2/s3/g4/script.js
@@ -1,15 +1,41 @@
 let apiKey = 'KTKjx0dwDeyoMDIgiLOsDSW1gwulInSop4toWR9vRVPnmDxJ9TusQHoh';
 
-let loadImg = document.getElementById('loadImg');
-let loadSecImg = document.getElementById('loadSecImg');
+const PIXELS_URL = 'https://api.pexels.com/v1/search?query='
 
+const renderImg = function (images) {
+    let row = document.querySelector('.album .container .row');
+    row.innerHTML = '';
+    images.forEach((images) => {
+        let col = ``
+    });
+}
 
-let nuovaImg =
-    fetch('https://api.pexels.com/v1/search?query=lakes', {
+const nuovaImg = function (query) {
+    fetch(+ query, {
         headers: {
             authorization: apiKey,
         },
     })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                return new Error("error")
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
 //non ho capito niente 
 
-loadImg = fetch('https://api.pexels.com/v1/search?query=lakes')
+window.onload = function () {
+    let loadImg = document.getElementById('loadImg');
+    loadImg.addEventListener('click', function () {
+        nuovaImg('lakes')
+    })
+    let loadSecImg = document.getElementById('loadSecImg');
+    loadSecImg.addEventListener('click', function () {
+        nuovaImg('mountains')
+    })
+}
